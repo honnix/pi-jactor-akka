207193161e751fc0fd74ca5545bd24973e537503
Daniel Seidler <daniel.seidler@gmail.com>
2013-01-04 22:01:04 +0100

    Readme added

---
+++ README.md
@@ -0,0 +1,12 @@
+#Pi Calculation using jActor and Akka#
+##Results##
+Workers  = 5
+Elements = 10000
+Messages = 10000
+
+Calculation Time in milliseconds @2.2GHz Intel Core 2 Duo
+
+jActor = 77015
+Akka   = 91878
+
+PI = 3.141592643589793403219407591351095649853664149020460172323510050773620605468750
--- src/main/java/VS.java
+++ src/main/java/VS.java
@@ -8,15 +8,16 @@ public class VS {
         PiAkka piAkka = new PiAkka();
         PijActor pijActor = new PijActor();
 
+        run(pijActor);
         run(piAkka);
         run(pijActor);
-
+        run(piAkka);
     }
 
     static void run(PiCalculator picalc) throws Exception {
         int nrOfWorkers = 5;
-        int nrOfElements = 100;
-        int nrOfMessages = 1000;
+        int nrOfElements = 10000;
+        int nrOfMessages = 10000;
 
         run(nrOfWorkers, nrOfElements, nrOfMessages, picalc);
     }
